26ce552c08371679c247c6efdc2d5257b0cf01a9
Bart van den Burg <bart@burgov.nl>
2012-04-11 11:20:27 +0200

    refactored HTTP

--- lib/Protocol/Protocol/HTTP.php
+++ lib/Protocol/Protocol/HTTP.php
@@ -2,45 +2,43 @@
 
 namespace Samson\Protocol\Protocol;
 
+use Samson\Protocol\Socket;
+
 class HTTP
 {
     private $s;
 
-    private $responseHeaders;
-
-    private $response;
-
-    public function __construct($url, $method = 'GET"', $headers = array(), $content = null)
+    public function request($url, $method = 'GET"', $headers = array(), $content = '')
     {
-        $this->s = new Samson\Protocol\Socket();
-        $this->s->connect($url, 80);
-
         $urlData = parse_url($url);
 
+        $this->s = new Socket();
+        $this->s->connect($urlData['host'], 80);
+
         if (!isset($headers['Host'])) {
-            $headers['Host'] = $urlData['host'];
+            $headers[] = 'Host: '.$urlData['host'];
         }
         if (strlen($content)) {
-            $headers['Content-length'] = strlen($content);
+            $headers[] = 'Content-length: '.strlen($content);
         }
-        
+
         $this->s->writeLine(sprintf('%s %s HTTP/1.1', $method, $urlData['path'].(isset($urlData['query']) ? '?'.$urlData['query'] : '')));
-            
-        foreach($headers as $key => $value) {
-            $s->writeln(sprintf("%s: %s", $key, $value));
+
+        foreach ($headers as $header) {
+            $this->s->writeLine($header);
         }
-        $s->writeln('');
-        
-        if (strlen($content)) {
-            $this->s->write($content);
-        }
+        $this->s->writeLine('');
+
+        $this->s->write($content);
+
+        return $this->parseResponse();
     }
 
     private function parseResponse()
     {
+        $response = $this->s->readLine()."\r\n";
 
         $responseHeaders = array();
-        $answer = $this->s->readLine();
         while ($l = $this->s->readLine()) {
             if (false === strpos($l, ": ")) {
                 var_dump($l);
@@ -48,38 +46,22 @@ class HTTP
             }
             list($key, $val) = explode(": ", $l);
             $responseHeaders[$key] = $val;
+            $response .= $l."\r\n";
         }
+        $response .= "\r\n";
 
-        $this->responseHeaders = $responseHeaders;
-
-        $response = $this->s->readCleanBuffer();
+        $content = $this->s->readCleanBuffer();
         if (array_key_exists('Content-Length', $responseHeaders)) {
-            while (strlen($response) < (int) $responseHeaders['Content-Length']) {
+            while (strlen($content) < (int) $responseHeaders['Content-Length']) {
                 $read = $this->s->read(128);
-                $response .= $read;
+                $content .= $read;
             }
         } else {
             while (false !== ($read = $this->s->read(128))) {
-                $response .= $read;
+                $content .= $read;
             }
         }
 
-        $this->response = $response;
-    }
-
-    public function getResponseHeaders()
-    {
-        if (null === $this->responseHeaders) {
-            $this->parseResponse();
-        }
-        return $this->responseHeaders;
-    }
-
-    public function getResponse()
-    {
-        if (null === $this->response) {
-            $this->parseResponse();
-        }
-        return $this->response;
+        return $response.$content;
     }
 }
